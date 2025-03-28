39833f9bd02dd15fa789baba0cc93624b217db3a
yufangling <helen831128@gmail.com>
2025-03-28 15:14:11 +0900

    cognitoリソースを追加

--- mooncat-cdk/bin/mooncat-cdk.ts
+++ mooncat-cdk/bin/mooncat-cdk.ts
@@ -1,11 +1,21 @@
 #!/usr/bin/env node
 import * as cdk from "aws-cdk-lib";
-import { MooncatCdkStack } from "../lib/mooncat-cdk-stack";
+import { UserStack } from "../lib/user-stack";
+import { CognitoStack  } from "../lib/cognito-stack";
+
+// 環境変数からアカウントとリージョンを取得
+const env = {
+  account: process.env.CDK_DEFAULT_ACCOUNT,
+  region: process.env.CDK_DEFAULT_REGION,
+};
 
 const app = new cdk.App();
-new MooncatCdkStack(app, "MooncatCdkStack", {
-  env: {
-    account: process.env.CDK_DEFAULT_ACCOUNT,
-    region: process.env.CDK_DEFAULT_REGION,
-  },
+
+// CognitoStack の作成
+const cognitoStack = new CognitoStack(app, 'CognitoStack', {
+  env, // env を渡す
+});
+new UserStack(app, "UserStack", {
+  cognitoStack: cognitoStack, // CognitoStack のインスタンスを渡す
+  env, // env を渡す
 });
---
+++ mooncat-cdk/lib/cognito-stack.ts
@@ -0,0 +1,57 @@
+import * as cdk from "aws-cdk-lib";
+import { Construct } from "constructs";
+
+import * as cognito from "aws-cdk-lib/aws-cognito";
+
+export class CognitoStack extends cdk.Stack {
+  public readonly userPool: cognito.UserPool;
+
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    this.userPool = new cognito.UserPool(this, "MoonCatUserPool", {
+      userPoolName: "MoonCatUserPool",
+      selfSignUpEnabled: true, // ユーザーがサインアップ可能
+      signInAliases: { email: true }, // メールアドレスでログイン
+      autoVerify: { email: true }, // メールアドレスの自動検証
+      standardAttributes: {
+        email: { required: true, mutable: true }, // メールアドレスを必須属性、変更可
+      },
+      passwordPolicy: {
+        minLength: 8,
+        requireUppercase: true,
+        requireLowercase: true,
+        requireDigits: true,
+        requireSymbols: false,
+      },
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+      accountRecovery: cognito.AccountRecovery.EMAIL_ONLY, //パスワード忘れ時にメールのみでアカウント回復
+    });
+
+    const userPoolClient = new cognito.UserPoolClient(
+      this,
+      "MoonCatUserPoolClient",
+      {
+        userPool: this.userPool,
+        userPoolClientName: "MoonCatUserPoolClient",
+        authFlows: {
+          userPassword: true, // ユーザー名 & パスワード認証
+          userSrp: true, // SRP (セキュアリモートパスワード) 認証
+        },
+        accessTokenValidity: cdk.Duration.hours(1), // アクセストークンの有効期限 (デフォルト 1時間)
+        idTokenValidity: cdk.Duration.hours(1), // IDトークンの有効期限 (デフォルト 1時間)
+        refreshTokenValidity: cdk.Duration.days(30), // リフレッシュトークンの有効期限 (デフォルト 30日)
+        preventUserExistenceErrors: true, // セキュリティ向上のための設定
+      }
+    );
+
+    // ユーザープールの ID とクライアント ID を出力
+    new cdk.CfnOutput(this, "UserPoolId", {
+      value: this.userPool.userPoolId,
+    });
+
+    new cdk.CfnOutput(this, "UserPoolClientId", {
+      value: userPoolClient.userPoolClientId,
+    });
+  }
+}
