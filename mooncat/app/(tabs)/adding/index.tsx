babf5da33b5efa10ce37baaf829244541e4df20a
yufangling <helen831128@gmail.com>
2025-04-02 17:25:43 +0900

    useContextでログイン後の画面遷移やセッション管理を実装

--- mooncat/app/(tabs)/adding/index.tsx
+++ mooncat/app/(tabs)/adding/index.tsx
@@ -19,11 +19,3 @@ export default function Tab() {
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-});
---
+++ mooncat/utils/auth.tsx
@@ -0,0 +1,108 @@
+import {
+    CognitoUserPool,
+    CognitoUser,
+    AuthenticationDetails,
+    CognitoUserAttribute,
+    ICognitoUserPoolData,
+} from 'amazon-cognito-identity-js';
+import { assertInputObjectType } from 'graphql';
+
+
+const UserPoolId = process.env.EXPO_PUBLIC_USERPOOL_ID || '';
+const ClientId = process.env.EXPO_PUBLIC_CLIENT_ID || '';
+
+const poolData: ICognitoUserPoolData = {
+    UserPoolId,
+    ClientId,
+};
+
+const userPool = new CognitoUserPool(poolData);
+
+export const getCurrentUserSession = (): Promise<CognitoUser | null> => {
+    return new Promise((resolve, reject) => {
+        const user = userPool.getCurrentUser();
+
+        if (!user) {
+            return reject(null);
+        }
+
+        user.getSession((err: any, session: any) => {
+            if (err || !session?.isValid()) {
+                return reject(null);
+            }
+
+            resolve(user);
+        });
+    });
+}
+
+export const signOut = (): Promise<void> => {
+    return new Promise((resolve) => {
+        const user = userPool.getCurrentUser();
+
+        if (user) {
+            user.signOut();
+        }
+
+        resolve();
+    });
+}
+
+export const signIn = (email: string, password: string): Promise<CognitoUser> => {
+    return new Promise((resolve, reject) => {
+        const user = new CognitoUser({ Username: email, Pool: userPool });
+        const authDetails = new AuthenticationDetails({ Username: email, Password: password });
+
+        user.authenticateUser(authDetails, {
+            onSuccess: (session) => {
+                const accessToken = session.getAccessToken().getJwtToken();
+                const idToken = session.getIdToken().getJwtToken();
+
+                console.log('Access Token:', accessToken);
+                console.log('ID Token:', idToken);
+
+                resolve(user)
+            },
+            onFailure: (err) => reject(err),
+        });
+    });
+}
+
+export const signUp = (email: string, password: string): Promise<void> => {
+    return new Promise((resolve, reject) => {
+        const emailAttribute = new CognitoUserAttribute({
+            Name: 'email',
+            Value: email,
+        });
+
+        const attributeList = [emailAttribute];
+
+        userPool.signUp(email, password, attributeList, [], (err, result) => {
+            if (err) {
+                console.log(err.message || JSON.stringify(err))
+                reject(err);
+            }
+            console.log(result);
+            resolve();
+        });
+    });
+}
+
+export const confirmSignUp = (email: string, confirmationCode: string): Promise<CognitoUser> => {
+    return new Promise((resolve, reject) => {
+        const user = new CognitoUser({
+            Username: email,
+            Pool: userPool,
+        });
+
+        user.confirmRegistration(confirmationCode, true, (err, result) => {
+            if (err) {
+                console.log(err);
+                reject();
+            }
+
+            console.log(result);
+            resolve(user);
+        });
+    });
+}
